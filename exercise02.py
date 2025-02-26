bcbd6731ad7dbb8195f67a90d0022e7107db701a
Mark Peters <mapeters101@earthlink.net>
2025-02-26 17:06:17 -0500

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,19 @@
+'''
+Question 2
+Given the following dictionaries:
+
+d1 = {'a': 10, 'b': 20, 'c': 30}
+d2 = {'d': 100, 'e': 200, 'f': 300}
+d3 = {'f': 30, 'g': 40}
+Construct two lists, one containing all the keys of both dictionaries, and one
+containing all the values of each dictionary. (It is OK for values or keys to
+be repeated in the lists).
+
+In the example above you should end up with two lists containing the elements:
+
+'a', 'b', 'c', 'd', 'e', 'f', 'f', 'g'
+and
+
+10, 20, 30, 100, 200, 300, 30, 40
+Note that the order in which the elements appear in each list is not important.
+'''
