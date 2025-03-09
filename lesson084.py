0ecb3bcaff67e04c94f49012e9ff701b89f8c365
Mark Peters <mapeters101@earthlink.net>
2025-03-09 16:20:00 -0400

    Work on lesson084 from 2:40 to 3:20.

--- lesson084.py
+++ lesson084.py
@@ -15,3 +15,24 @@ print()
 l = [1,2,3,4]
 print(3 in l)
 print(10 not in l)
+print()
+
+data.clear()
+print(data)
+print(len(data))
+
+data = {
+    'open': 100,
+    'high': 110,
+    'low': 95,
+    'close': 110,
+}
+print(data)
+print(len(data))
+print('-'*80)
+
+
+
+
+
+
