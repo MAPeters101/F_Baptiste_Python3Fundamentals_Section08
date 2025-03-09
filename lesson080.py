03b3a807c5db9edf779259612366fd2a46a9fccb
Mark Peters <mapeters101@earthlink.net>
2025-03-08 23:09:25 -0500

    Work on lesson080 from 4:10 to 5:40.

--- lesson080.py
+++ lesson080.py
@@ -20,4 +20,15 @@ d = {3.14: 'pi', 2: 'even', 'prime':7}
 print(d)
 print(d[3.14])
 print(d[2])
+print()
+
+
+l = [1, 2, 3]
+#d = {l: 100}
+print(hash(100))
+print(hash(3.14))
+#print(hash(l))
+print(hash(1))
+
+
 
