3dbedd7eed0206933b057af11da24ac7e07d7e0d
Mark Peters <mapeters101@earthlink.net>
2025-03-09 16:17:46 -0400

    Work on lesson084 to 2:40.

---
+++ lesson084.py
@@ -0,0 +1,17 @@
+
+data = {
+    'open': 100,
+    'high': 110,
+    'low': 95,
+    'close': 110,
+}
+
+print('open' in data)
+print(3.14 in data)
+print('open' not in data)
+print('x' not in data)
+print()
+
+l = [1,2,3,4]
+print(3 in l)
+print(10 not in l)
