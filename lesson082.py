de887d228b020b323fe0eb506f4295e384b0951b
Mark Peters <mapeters101@earthlink.net>
2025-03-09 16:10:15 -0400

    Complete work on lesson082 from 4:20.

--- lesson082.py
+++ lesson082.py
@@ -31,4 +31,23 @@ for key, value in d.items():
     print(f'd[{key}] = {value}')
 print()
 
+d['x'] = 100
+print(d)
+
+for k in d:
+    print(k)
+print()
+
+d['key 1'] = 100
+for k in d:
+    print(k)
+print()
+
+del d['key 1']
+print(d)
+d['key 1'] = 100
+for k in d:
+    print(k)
+print()
+
 
