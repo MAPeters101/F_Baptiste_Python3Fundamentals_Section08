d3a8a48481accc6d6970587dc9e12f5e2bcf9ceb
Mark Peters <mapeters101@earthlink.net>
2025-03-08 22:53:59 -0500

    Work on lesson080 to 2:20.

---
+++ lesson080.py
@@ -0,0 +1,13 @@
+d = {'a':1, 'b':2, 'c':3}
+print(d)
+
+person = {
+    'first_name': 'Eric',
+    'last_name': 'Idle',
+    'year_born': 2016
+}
+print(person['year_born'])
+person['year_born'] = 1943
+print(person)
+
+
