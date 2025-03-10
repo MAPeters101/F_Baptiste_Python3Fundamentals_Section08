e6d91a39a0afb4cbc9c60b9957da70eb60c3577a
Mark Peters <mapeters101@earthlink.net>
2025-03-09 20:09:48 -0400

    Work on lesson084 from 15:45 to 24:00.

--- lesson084.py
+++ lesson084.py
@@ -142,4 +142,42 @@ print(d2)
 print('-'*80)
 
 
+transactions = [
+    {'item': 'widget', 'trans_type': 'sale', 'quantity': 10},
+    {'item': 'widget', 'trans_type': 'sale', 'quantity': 5},
+    {'item': 'widget', 'trans_type': 'refund', 'quantity': 2},
+    {'item': 'license', 'trans_type': 'sale', 'quantity': 1},
+    {'item': 'license', 'trans_type': 'sale', 'quantity': 1},
+    {'item': 'license', 'trans_type': 'refund', 'quantity': 1},
+]
+
+total_sold = {}
+for transaction in transactions:
+    item = transaction['item']
+    is_sale = True if transaction['trans_type'] == 'sale' else False
+    quantity = transaction['quantity']
+
+    if is_sale:
+        if item in total_sold:
+            total_sold[item] += quantity
+        else:
+            total_sold[item] = quantity
+
+print(total_sold)
+
+net_sales = {}
+for transaction in transactions:
+    item = transaction['item']
+    is_sale = True if transaction['trans_type'] == 'sale' else False
+    quantity = transaction['quantity']
+
+    if not is_sale:
+        quantity = -quantity
+
+    if item in net_sales:
+        net_sales[item] += quantity
+    else:
+        net_sales[item] = quantity
+
+print(net_sales)
 
